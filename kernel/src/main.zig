495f6c10d5ecb2cc193b5793d66301c1af9d925c
Arnau478 <arnauxabia@gmail.com>
2024-02-26 17:15:30 +0100

    Logging system rework

--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -7,20 +7,29 @@ pub const os = @import("os.zig");
 const log = std.log.scoped(.core);
 
 pub fn logFn(comptime message_level: std.log.Level, comptime scope: @Type(.EnumLiteral), comptime format: []const u8, args: anytype) void {
-    var log_allocator_buf: [2048]u8 = undefined;
+    var log_allocator_buf: [4096 * 8]u8 = undefined;
     var log_fba = std.heap.FixedBufferAllocator.init(&log_allocator_buf);
     const log_allocator = log_fba.allocator();
 
-    const msg = std.fmt.allocPrint(log_allocator, switch (message_level) {
+    const prefix = switch (message_level) {
         .info => "\x1b[34m",
         .warn => "\x1b[33m",
         .err => "\x1b[31m",
         .debug => "\x1b[90m",
-    } ++ "[" ++ @tagName(message_level) ++ "]\x1b[0m (" ++ @tagName(scope) ++ ") " ++ format ++ "\n", args) catch "LOG_FN_OOM";
+    } ++ "[" ++ @tagName(message_level) ++ "]\x1b[0m (" ++ @tagName(scope) ++ ")";
+
+    const msg = std.fmt.allocPrint(log_allocator, prefix ++ " " ++ format, args) catch "\x1b[31m\x1b[1m!!!LOG_FN_OOM!!!\x1b[0m";
 
     for (msg) |char| {
         hal.debugcon(char);
+        if (char == '\n') {
+            for (0..prefix.len - 10) |_| hal.debugcon(' ');
+            hal.debugcon('|');
+            hal.debugcon(' ');
+        }
     }
+
+    hal.debugcon('\n');
 }
 
 pub const std_options = .{
